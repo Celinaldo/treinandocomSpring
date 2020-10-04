03f75c014bf36440ab0b1ac51af809242a24513a
Celinaldo Alves Ferreira Junior <https://github.com/Celinaldo>
2020-10-04 15:52:48 -0300

    first commit

---
+++ src/main/java/com/example/demo/SpringClient1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClient1Application.class, args);
+	}
+
+}
